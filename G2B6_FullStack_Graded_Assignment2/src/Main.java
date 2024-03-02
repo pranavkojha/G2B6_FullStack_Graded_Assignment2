db7628dd4907e9308434a0972ed0697cd3adedb9
Pranav Kumar Ojha <pranavkojha@gmail.com>
2024-03-02 22:12:39 +0530

    Merge code

--- G2B6_FullStack_Graded_Assignment2/src/Main.java
+++ G2B6_FullStack_Graded_Assignment2/src/Main.java
@@ -1,41 +1,77 @@
-import java.util.*;
+/**
+ * Group assigment 2
+ *
+ * @author Pranav Kumar Ojha, Faisal Raza Khan, Haritha, Shreya Singh, Prajna
+ *
+ * Q1. This class show how to plan floor construction for skyscraper.
+ *
+ * Q2. This class shows how to convert a balanced BST to a Skewed tree.
+ *
+ * Example -
+ *
+ * Input:
+ *                   50
+ *                 /    \
+ *                30     60
+ *               / \    /
+ *              10 20   55
+ *
+ * Output:
+ *      10
+ *       \
+ *        20
+ *         \
+ *          30
+ *           \
+ *            50
+ *             \
+ *              55
+ *               \
+ *                60
+ */
+
+import com.bst.BinarySearchTree;
+import com.bst.Node;
+import com.floor.FloorPlan;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the total number of floors in the building: ");
-        int N = scanner.nextInt();
-        List<Integer> floors = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            System.out.print("Enter the floor size given on day " + (i + 1) + ": ");
-            int size = scanner.nextInt();
-            floors.add(size);
-        }
-        List<Integer> assembledFloors = constructSkyscraper(N, floors);
-        System.out.println("The order of construction is as follows:");
-        int day = 1;
-        for (int floorSize : assembledFloors) {
-            if (floorSize != 0) {
-                System.out.println("Day: " + day + " " + floorSize);
-                day++;
-            } else {
-                System.out.println("Day: " + day);
-                day++;
-            }
-        }
+        // Test BST code.
+        System.out.println("**************************");
+        System.out.println("Testing skewed BST code");
+        testBST();
+        System.out.println("**************************\n");
+
+        // Testing floor plan.
+        System.out.println("**************************");
+        System.out.println("Testing floor plan");
+        testFloor();
+        System.out.println("**************************\n");
     }
-    public static List<Integer> constructSkyscraper(int N, List<Integer> floors) {
-        List<Integer> assembledFloors = new ArrayList<>();
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-        for (int i = 0; i < N; i++) {
-            int currentFloorSize = floors.get(i);
-            priorityQueue.add(currentFloorSize);
-            while (!priorityQueue.isEmpty() && priorityQueue.peek() == N - i) {
-                assembledFloors.add(priorityQueue.poll());
-                N--;
-            }
-        }
-        for (int i = 0; i < N; i++) 
-            assembledFloors.add(0);
-        return assembledFloors;
+    public static void testFloor() {
+        //Integer[] input = {3,1,2};
+        FloorPlan fp = new FloorPlan();
+        fp.printFloorPlan();
+    }
+    public static void testBST() {
+        // Create BST
+        Node Tree = new Node(50);
+        BinarySearchTree bst = new BinarySearchTree(Tree);
+        bst.insertNode(30);
+        bst.insertNode(60);
+        bst.insertNode(10);
+        bst.insertNode(20);
+        bst.insertNode(55);
+        System.out.println("Binary Tree created: \n" + bst.getRoot() + "\n");
+
+        // Print skewed tree with sorted method.
+        BinarySearchTree skewed_bst = new BinarySearchTree();
+        skewed_bst = bst.getRightSkewedTree("sort");
+        System.out.println(" Sorted BST Method 1: \n" + skewed_bst.getRoot());
+
+        // Print skewed tree using in-order traversal method.
+        skewed_bst = bst.getRightSkewedTree("inorder");
+        System.out.println("Inorder BST Method 2: \n" + skewed_bst.getRoot());
     }
 }
--- G2B6_FullStack_Graded_Assignment2/src/com/floor/FloorPlan.java
+++ G2B6_FullStack_Graded_Assignment2/src/com/floor/FloorPlan.java
@@ -1,4 +1,73 @@
 package com.floor;
 
+import java.util.*;
+
 public class FloorPlan {
+    private Integer floors;
+    private Integer[] floorSize;
+    
+    // Constructors.
+    public FloorPlan() {
+        // Accept floors.
+        System.out.println("Enter total no. of floors in the building:");
+        Scanner in = new Scanner(System.in);
+        this.floors = in.nextInt();
+        
+        // Accept size on given day
+        floorSize = new Integer[this.floors];
+        for(int i=0; i < this.floors; i++) {
+            System.out.println("\tEnter the floor size given on day " + (i+1) + ": ");
+            this.floorSize[i] = in.nextInt();
+        }
+    }
+
+    public FloorPlan(int floors) {
+        // Accept floors.
+        this.floors = floors;
+        Scanner in = new Scanner(System.in);
+
+        // Accept size on given day
+        floorSize = new Integer[this.floors];
+        for(int i=0; i < this.floors; i++) {
+            System.out.println("\tEnter the floor size given on day " + (i+1) + ": ");
+            this.floorSize[i] = in.nextInt();
+        }
+    }
+    public FloorPlan(int floors, Integer[] floorSize) {
+        // Accept floors.
+        this.floors = floors;
+        this.floorSize = floorSize;
+    }
+
+    public void printFloorPlan() {
+        List<Integer> assembledFloors = this.constructSkyscraper();
+        System.out.println("The order of construction is as follows:");
+        int day = 1;
+        for (int floorSize : assembledFloors) {
+            if (floorSize != 0) {
+                System.out.println("Day: " + day + " " + floorSize);
+                day++;
+            } else {
+                System.out.println("Day: " + day);
+                day++;
+            }
+        }
+    }
+
+    private List<Integer> constructSkyscraper() {
+        List<Integer> assembledFloors = new ArrayList<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for (int i = 0; i < this.floors; i++) {
+            int currentFloorSize = this.floorSize[i];
+            priorityQueue.add(currentFloorSize);
+            while (!priorityQueue.isEmpty() && priorityQueue.peek() == this.floors - i) {
+                assembledFloors.add(priorityQueue.poll());
+                this.floors--;
+            }
+        }
+        for (int i = 0; i < this.floors; i++)
+            assembledFloors.add(0);
+        return assembledFloors;
+    }
+
 }
